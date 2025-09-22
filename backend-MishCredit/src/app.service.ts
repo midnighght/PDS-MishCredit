3500b28f3799c749ee63896b59e00f1b34591dc8
BenjaminCuello <benjamin.cuello@alumnos.ucn.cl>
2025-09-22 16:52:44 -0300

    d

--- backend-MishCredit/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-// filepath: c:\Users\Pro360\Documents\Workspace\JS-HTML-CSS\PDS-MishCredit\backend-MishCredit\src\app.service.ts
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'MishCredit Proxy Server - Ready to proxy UCN API calls!';
-  }
-}
--- frontend-MishCredit/src/hooks/useAuth.hook.ts
+++
@@ -1,5 +0,0 @@
-import { useAuthContext } from '../contexts/auth.context';
-
-export const useAuth = () => {
-  return useAuthContext();
-};
